f23c1fe0365b72f1e0df7e43cc61b89608c056e7
김지원 <wldnjs35242@naver.com>
2024-04-15 08:45:23 +0900

    :art: Feat: 사이드바 gsap 애니메이션 적용

--- Frontend/src/components/Layout/Sidebar.tsx
+++ Frontend/src/components/Layout/Sidebar.tsx
@@ -1,38 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import logoImg from "../../assets/react.svg";
-import { Link } from "react-router-dom";
-import { RxDashboard } from "react-icons/rx";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineHome, AiOutlineFund, AiOutlineBook } from "react-icons/ai";
+import { gsap } from "gsap";
 
-const SidebarContainer = styled.div<{ sidebarOpen: boolean }>`
+const SidebarContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   padding: 2rem 2rem;
-  width: ${(props) => (props.sidebarOpen ? "15rem" : "3rem")};
-  height: 100vh;
-  background-color: #e8e8e8;
+  width: 15rem;
+  height: 100%;
+  background-color: white;
   transition: width 0.5s ease;
-
-  @keyframes collapseSidebar {
-    from {
-      width: 15rem;
-    }
-    to {
-      width: 3rem;
-    }
-  }
-
-  animation: ${(props) =>
-    props.sidebarOpen ? "none" : `collapseSidebar 0.5s ease`};
+  box-shadow: 1px 0px 5px 1px #67676755;
 `;
 
 const Logo = styled.div`
   display: flex;
   margin-bottom: 4rem;
+  height: 100px;
+  align-items: center;
   img {
-    width: 50px;
-    margin-right: 10px;
+    width: 30px;
+  }
+  h2 {
+    margin-left: 15px;
   }
 `;
 
@@ -54,15 +48,18 @@ const ProfileContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-bottom: 5rem;
+  margin-bottom: 10vh;
+  height: 40px;
   img {
-    width: 50px;
-    margin-right: 20px;
+    width: 30px;
   }
 `;
 const ProfileInfo = styled.div`
   display: flex;
   flex-direction: column;
+  height: 50px;
+  margin-left: 15px;
+  justify-content: center;
 `;
 
 const ContentContainer = styled.div``;
@@ -71,16 +68,18 @@ const SideLinkContainer = styled.div`
   margin: 50px 0;
   display: flex;
   align-items: center;
+  color: #949494;
   :hover {
-    box-shadow: inset 0 0 0 1px #e5e5e5;
+    color: black;
   }
 `;
 
-const SideLink = styled(Link)`
+const SideLink = styled(Link)<{ $isActive: boolean }>`
   display: flex;
   align-items: center;
   text-decoration: none;
-  color: inherit;
+  width: 100%;
+  color: ${(props) => (props.$isActive ? "black" : "inherit")};
   font-size: 16px;
 `;
 
@@ -88,7 +87,6 @@ const SideLinkIcon = styled.div`
   display: flex;
   margin-right: 10px;
   font-size: 30px;
-  border: solid 1px black;
 `;
 
 const SideLinkLabel = styled.span`
@@ -103,31 +101,60 @@ interface SidebarProps {
 }
 
 function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
+  const location = useLocation();
+  const logoTextRef = useRef(null);
+  const sidebarRef = useRef(null);
+  const profileTextRef = useRef(null);
+  const sideLinkLabelRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    const list = [
+      logoTextRef.current,
+      profileTextRef.current,
+      ...sideLinkLabelRefs.current,
+    ];
+
+    if (sidebarOpen) {
+      tl.to(sidebarRef.current, { width: "15rem", duration: 0.01 })
+        .to(logoTextRef.current, {})
+        .set(list, { display: "block" })
+        .to(list, { opacity: 1 });
+    } else {
+      tl.to(list, { opacity: 0, duration: 0.01 })
+        .set(list, { display: "none" })
+        .to(sidebarRef.current, { width: "2rem" })
+        .totalDuration(0.01);
+    }
+  }, [sidebarOpen]);
+
   return (
-    <SidebarContainer sidebarOpen={sidebarOpen}>
+    <SidebarContainer ref={sidebarRef}>
       <Logo>
         <img src={logoImg} />
-        {sidebarOpen ? <h2>프로젝트이름</h2> : ""}
+        <h2 ref={logoTextRef}>프로젝트이름</h2>
       </Logo>
       <CloseButtonContainer onClick={() => setSidebarOpen(!sidebarOpen)}>
         {sidebarOpen ? "X" : "-"}
       </CloseButtonContainer>
       <ProfileContainer>
         <img src={logoImg}></img>
-        {sidebarOpen && (
-          <ProfileInfo>
-            <div>UserName</div>
-            <div>YES24 중고서점 목동점</div>
-          </ProfileInfo>
-        )}
+        <ProfileInfo ref={profileTextRef}>
+          <div>UserName</div>
+          <div>YES24 중고서점 목동점</div>
+        </ProfileInfo>
       </ProfileContainer>
 
       <ContentContainer>
-        {linksArray.map(({ icon, label, to }) => (
+        {linksArray.map(({ icon, label, to }, index) => (
           <SideLinkContainer key={label}>
-            <SideLink to={to}>
+            <SideLink to={to} $isActive={location.pathname == to}>
               <SideLinkIcon>{icon}</SideLinkIcon>
-              {sidebarOpen && <SideLinkLabel>{label}</SideLinkLabel>}
+              <SideLinkLabel
+                ref={(ref) => (sideLinkLabelRefs.current[index] = ref)}
+              >
+                {label}
+              </SideLinkLabel>
             </SideLink>
           </SideLinkContainer>
         ))}
@@ -135,21 +162,20 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
     </SidebarContainer>
   );
 }
-
 const linksArray = [
   {
     label: "Dashboard",
-    icon: <RxDashboard />,
-    to: "/main/page1",
+    icon: <AiOutlineHome />,
+    to: "/main",
   },
   {
     label: "Trending Keywords",
-    icon: <RxDashboard />,
+    icon: <AiOutlineFund />,
     to: "/main/page2",
   },
   {
     label: "Daily Key Reads",
-    icon: <RxDashboard />,
+    icon: <AiOutlineBook />,
     to: "/main/page3",
   },
 ];
