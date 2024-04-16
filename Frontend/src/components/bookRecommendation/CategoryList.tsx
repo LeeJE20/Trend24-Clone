9922d3124e2a694e8ba5285e95e298ba1fb3fee9
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-04-17 01:01:23 +0900

    :art: Feat: 검색 박스

--- Frontend/src/components/bookRecommendation/CategoryList.tsx
+++ Frontend/src/components/bookRecommendation/CategoryList.tsx
@@ -1,29 +1,63 @@
 // import React, { useState } from "react";
 import styled from "styled-components";
+import { IoSearchSharp } from "react-icons/io5";
 
 const CategoryList = ({ listData }: { listData: string[] }) => {
   console.log(listData);
-  
+
   return (
     <Container>
       <SearchBox>
-        
+        <input />
+        <button>
+          <IoSearchSharp />
+        </button>
       </SearchBox>
-
+      <FilterBox></FilterBox>
     </Container>
   );
 };
 
-
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-flow: column;
   padding: 30px;
 `;
 
 const SearchBox = styled.div`
-  border: 1px solid black;
+  display: flex;
+  width: 100%;
   height: 50px;
+  border: 2px solid black;
+  border-radius: 15px;
+  input {
+    height: 100%;
+    width: 80%;
+    border-radius: 15px;
+    background-color: transparent;
+    border: none;
+    padding: 3% 8%;
+    box-sizing: border-box;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    cursor: pointer;
+    background-color: transparent;
+    border: transparent;
+    font-size: 100%;
+    height: 100%;
+    width: 20%;
+  }
+`;
+
+const FilterBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  border: 2px solid black;
+  border-radius: 15px;
 `;
 
 export default CategoryList;
--- Frontend/src/pages/BookRecPage.tsx
+++ Frontend/src/pages/BookRecPage.tsx
@@ -3,31 +3,25 @@ import styled from "styled-components";
 import { categoryListData } from "../constants/DummyData";
 import { Mobile } from "../constants/Display";
 
-
 const BookRecPage = () => {
   return (
-  <Body>
-    <CategoryListContainer>
-      <CategoryList listData={categoryListData}/>
-    </CategoryListContainer>
-    <CategoryKeywordContainer>
-
-    </CategoryKeywordContainer>
-    <BookListContainer>
-
-    </BookListContainer>
-  </Body>    
+    <Body>
+      <CategoryListContainer>
+        <CategoryList listData={categoryListData} />
+      </CategoryListContainer>
+      <CategoryKeywordContainer></CategoryKeywordContainer>
+      <BookListContainer></BookListContainer>
+    </Body>
   );
 };
-
-
 
 const Body = styled.div`
   display: grid;
   grid-template-columns: 1fr 3.5fr;
   grid-template-rows: 1fr 3fr;
-  grid-template-areas: "c k k"
-                       "c b b";
+  grid-template-areas:
+    "c k k"
+    "c b b";
   gap: 20px;
   width: 100%;
   height: 100%;
@@ -35,31 +29,32 @@ const Body = styled.div`
   ${Mobile} {
     grid-template-columns: 1fr;
     grid-template-rows: 1fr 1fr 1fr;
-    grid-template-areas: "c"
-                         "k"
-                         "b";
+    grid-template-areas:
+      "c"
+      "k"
+      "b";
   }
-`; 
+`;
 
 const CategoryListContainer = styled.div`
   grid-area: c;
   background-color: #ffffff;
   box-shadow: 1px 0px 5px 1px #67676755;
   border-radius: 20px;
-`; 
+`;
 
 const CategoryKeywordContainer = styled.div`
   grid-area: k;
   background-color: #ffffff;
   box-shadow: 1px 0px 5px 1px #67676755;
   border-radius: 20px;
-`; 
+`;
 
 const BookListContainer = styled.div`
   grid-area: b;
   background-color: #ffffff;
   box-shadow: 1px 0px 5px 1px #67676755;
   border-radius: 20px;
-`; 
+`;
 
 export default BookRecPage;
