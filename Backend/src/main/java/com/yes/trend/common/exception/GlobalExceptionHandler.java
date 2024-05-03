c4f9823ae5cd6f53ee845b855f10d0c94b0e8a5b
이정은 <silver36c@gmail.com>
2024-05-03 14:16:52 +0900

    ✨ Feat: 에러 처리

---
+++ Backend/src/main/java/com/yes/trend/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.yes.trend.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import com.yes.trend.common.dto.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+	private final StringBuilder sb = new StringBuilder();
+
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException customException) {
+		log.error("Custom Exception 발생!", customException);
+
+		String stringData =
+			customException.getData() == null ? null : customException.getData().toString();
+		ApiResponse<String> response = ApiResponse.error(customException.getErrorCode(),
+			stringData);
+		return ResponseEntity.badRequest().body(response);
+	}
+
+
+	@ExceptionHandler(Exception.class)
+	protected final ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception ex) {
+		log.error("Exception 발생!", ex);
+
+		ApiResponse<String> response = ApiResponse.globalError(HttpStatus.INTERNAL_SERVER_ERROR,
+			ex.getMessage());
+
+		return ResponseEntity.badRequest().body(response);
+	}
+}
+
