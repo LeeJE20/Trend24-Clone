d24affb557ed5e5e8787aa9b0458a3f713ae628a
이정은 <silver36c@gmail.com>
2024-05-03 12:56:18 +0900

    ✨ Feat: db 도커 세팅

---
+++ docker/trend-db/docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# .env 파일의 경로를 환경 변수로 설정
+# export ENV_FILE_PATH="./../../src/main/resources/.env"
+export ENV_FILE_PATH="./.env"
+
+# Docker Compose 실행
+docker-compose -f docker-compose.yml --env-file $ENV_FILE_PATH up
