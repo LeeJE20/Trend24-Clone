0406397a57069c69c4fadb3ca9a7b0196578ea06
이정은 <silver36c@gmail.com>
2024-05-08 08:18:06 +0900

    ✨ Feat: 책 추천 리스트 결과 DTO

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,4 +1,32 @@
 package com.yes.trend.api.recommend.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.yes.trend.common.dto.PageInfoDto;
+import com.yes.trend.domain.book.dto.BookDto;
+import com.yes.trend.domain.keyword.dto.KeywordDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class RecommendDto {
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class Response {
+		private PageInfoDto pageInfo;
+		private List<BookWithKeywords> list;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class BookWithKeywords extends BookDto {
+		private List<KeywordDto> keywords;
+	}
 }
