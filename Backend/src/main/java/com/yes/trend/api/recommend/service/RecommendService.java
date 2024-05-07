effdaf455bb107e5b123d9f8c331c64f78435a59
이정은 <silver36c@gmail.com>
2024-05-07 17:58:45 +0900

    ✨ Feat: Pageable 테스트

--- Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
@@ -1,7 +1,13 @@
 package com.yes.trend.api.recommend.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.yes.trend.domain.book.repository.BookRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,4 +17,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Transactional(readOnly = true)
 public class RecommendService {
+	private final BookRepository bookRepository;
+	public Object getRecommendedBooksByKeywordIds(List<Integer> list, int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return bookRepository.findAll(pageable);
+
+	}
 }
--- Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
+++ Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.yes.trend.domain.book.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yes.trend.domain.book.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+	Page<Book> findAll(Pageable pageable);
 }
