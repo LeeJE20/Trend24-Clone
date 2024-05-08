8ea676be8291e46f43b7c0c124c1b9c620350031
이정은 <silver36c@gmail.com>
2024-05-08 16:22:36 +0900

    ✨ Feat: 페이지네이션 없이 추천된 책 리스트 가져오기

--- Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.yes.trend.api.recommend.dto.KeywordWithBookDto;
 import com.yes.trend.domain.book.repository.BookRepository;
+import com.yes.trend.domain.recommendkeyword.repository.RecommendKeywordRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly = true)
 public class RecommendService {
 	private final BookRepository bookRepository;
-	public Object getRecommendedBooksByKeywordIds(List<Integer> list, int page, int size) {
+	private final RecommendKeywordRepository recommendKeywordRepository;
+
+	public Object getRecommendedBooksByKeywordIds(List<Integer> keywordIds, int page, int size) {
 		Pageable pageable = PageRequest.of(page, size);
-		return bookRepository.findAll(pageable);
+
+		List<KeywordWithBookDto> keywordWithBookDtos = recommendKeywordRepository.findBooksByKeywordIds(keywordIds);
+
+		return null;
 
 	}
 }
--- Backend/src/main/java/com/yes/trend/domain/recommendkeyword/repository/RecommendKeywordRepository.java
+++ Backend/src/main/java/com/yes/trend/domain/recommendkeyword/repository/RecommendKeywordRepository.java
@@ -1,8 +1,22 @@
 package com.yes.trend.domain.recommendkeyword.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.yes.trend.api.recommend.dto.KeywordWithBookDto;
 import com.yes.trend.domain.recommendkeyword.entity.RecommendKeyword;
 
 public interface RecommendKeywordRepository extends JpaRepository<RecommendKeyword, Integer> {
+	@Query(
+		"SELECT NEW com.yes.trend.api.recommend.dto.KeywordWithBookDto(k.name, b.id, b.productId, b.productName, b.categoryName, b.searchKeyword, b.totalClickCount, b.totalOrderCount, b.totalOrderAmount, b.salePrice, b.contents, b.totalPurchaseCount)"
+			+
+			" FROM RecommendKeyword rk " +
+			" JOIN rk.keyword k " +
+			" JOIN rk.dailyRecommend dr " +
+			" JOIN dr.book b " +
+			" WHERE k.id IN :keywordIds")
+	List<KeywordWithBookDto> findBooksByKeywordIds(@Param("keywordIds") List<Integer> keywordIds);
 }
