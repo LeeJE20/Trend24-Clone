859251b580440b6e3f1cb31f9d514faacc197977
이정은 <silver36c@gmail.com>
2024-05-12 22:59:51 +0900

    🔧 Chore: FastAPI 도커로 실행

---
+++ docker/trend-fastapi/build-requirements-image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 스크립트 실행 전에 현재 위치를 저장합니다.
+original_dir=$(pwd)
+
+# 스크립트 실행 위치를 Backend 디렉토리로 변경합니다.
+cd $(dirname "$0")/../../PythonAPI/pythonProject
+
+# .env 파일을 읽어 환경 변수로 설정합니다.
+source .env
+
+
+IMAGE_NAME=trend-fastapi-requirements
+FILE_NAME=Dockerfile-requirements
+
+
+# 현재 폴더의 Dockerfile을 사용하여 이미지를 빌드합니다.
+docker build -f $FILE_NAME -t $IMAGE_NAME .
+
+# 컨테이너를 실행합니다.
+docker run -d -p $PORT:8000 --name $CONTAINER_NAME --network trend-network $IMAGE_NAME
+
+echo "스크립트 끝"
