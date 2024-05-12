bfffc54b3826fb19e00963206da33b7c5a11d2d6
이정은 <silver36c@gmail.com>
2024-05-13 02:59:12 +0900

    ✨ Feat: 서랍 보기 방식 변경

--- Backend/src/main/java/com/yes/trend/api/drawer/service/DrawerService.java
+++ Backend/src/main/java/com/yes/trend/api/drawer/service/DrawerService.java
@@ -9,6 +9,7 @@ import com.yes.trend.api.drawer.dto.DrawerDto;
 import com.yes.trend.common.costants.ErrorCode;
 import com.yes.trend.common.exception.CustomException;
 import com.yes.trend.domain.admin.entity.Admin;
+import com.yes.trend.domain.admin.repository.AdminRepository;
 import com.yes.trend.domain.admin.service.AdminService;
 import com.yes.trend.domain.box.entity.Box;
 import com.yes.trend.domain.box.repository.BoxRepository;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly = true)
 public class DrawerService {
 	private final BoxRepository boxRepository;
+	private final AdminRepository adminRepository;
 	private final AdminService adminService;
 
 	@Transactional
@@ -33,5 +35,12 @@ public class DrawerService {
 		Box box = new Box(drawerPost.getName(), admin);
 		box = boxRepository.save(box);
 		return new DrawerDto.Response(box.getId(), box.getName(), new ArrayList<>());
+	}
+
+	@Transactional
+	public DrawerDto.Layout patchDrawerLayout(DrawerDto.Layout layout) {
+		Admin admin = adminService.getLoginAdmin();
+		admin.setLayout(layout.getLayout());
+		return new DrawerDto.Layout(admin.getLayout());
 	}
 }
