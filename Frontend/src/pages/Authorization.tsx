d2fe7e743d2c0f91979d74187c9e417d57eb011c
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 23:05:10 +0900

    :sparkles: Feat: 로그인 기능 완료

--- Frontend/src/pages/Authorization.tsx
+++ Frontend/src/pages/Authorization.tsx
@@ -1,192 +1,221 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { FaKey } from "react-icons/fa";
+import { signIn } from "../apis/authApi";
+import { useNavigate } from "react-router-dom";
 
-import Colors from "../constants/Color";
+const Login = () => {
+  const navigate = useNavigate();
 
-import react from "../assets/react.svg";
+  const [userId, setUserId] = useState(""); // ID 상태
+  const [password, setPassword] = useState(""); // 비밀번호 상태
 
-const Login: React.FC = () => {
-  const handleLogin = () => {
-    console.log("login");
+  // ID 입력 상자 변경 이벤트 핸들러
+  const handleUserIdChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUserId(value); // ID 상태 업데이트
   };
 
+  // 비밀번호 입력 상자 변경 이벤트 핸들러
+  const handlePasswordChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setPassword(value); // ID 상태 업데이트
+  };
+
+  // 로그인 버튼 클릭 이벤트 핸들러
+  const handleLogin = async () => {
+    try {
+      const res = await signIn(userId, password); // signIn 함수의 처리를 기다림
+      alert("로그인 성공");
+      localStorage.setItem("accessToken", res.accessToken);
+      navigate("/main");
+    } catch (error) {
+      alert("로그인 실패");
+      console.log(error);
+    }
+  };
   return (
-    <LoginPageContainer>
-      <LoginContainer>
-        <LoginImageWrapper>
-          <CompanyLogo>
-            <LogoImg src={react} alt="react" />
-            Trend24
-          </CompanyLogo>
-          <LogoTitle>
-            당신의 성장이
+    <Container>
+      <LoginFormContainer>
+        <LoginFormRightSide>
+          <div className="title">
+            <img className="logo" src="/Image/Logo/logo2.png" />
+          </div>
+          <div className="description">
+            환영합니다!
             <br />
-            시작되는 곳
-          </LogoTitle>
-          <LogoSubtitle>
-            관리자 페이지 관리자는
+            트렌드 기반 도서 추천 플랫폼입니다. <br />
             <br />
-            김지원 입니다
-          </LogoSubtitle>
-          <DummyImg src="Image/Dummy/DummyLogin.jpg" alt="login" />
-        </LoginImageWrapper>
-        <LoginFormWrapper>
-          <LoginForm>
-            <LoginTitle>Login</LoginTitle>
-            <LoginSmall>welcome to office</LoginSmall>
-            <LoginInput placeholder="Id" />
-            <LoginInput placeholder="Password" />
-            <LoginButton onClick={handleLogin}>Login</LoginButton>
-            <LoginOptions>
-              <RememberCheckBox>
-                <CheckBox></CheckBox>
-                remember me
-              </RememberCheckBox>
-              <ForgotPassword>forgot password</ForgotPassword>
-            </LoginOptions>
-            <Empty></Empty>
-          </LoginForm>
-        </LoginFormWrapper>
-      </LoginContainer>
-    </LoginPageContainer>
+            각종 카테고리의 트렌드 키워드와 <br />
+            관련 도서 리스트를 제공하여 <br />
+            도서 트렌드를 놓치지 않도록 도와드립니다.
+          </div>
+        </LoginFormRightSide>
+
+        <LoginFormLeftSide>
+          <LoginInputContainer>
+            <LoginInputWrap>
+              <FaRegEnvelope className="icon" />
+              <input
+                placeholder="ID"
+                type="text"
+                value={userId} // ID 입력 상자에 상태 값 연결
+                onChange={handleUserIdChange} // ID 입력 변경 이벤트 핸들러
+              />
+            </LoginInputWrap>
+            <LoginInputWrap>
+              <FaKey className="icon" />
+              <input
+                placeholder="Password"
+                type="password"
+                value={password} // 비밀번호 입력 상자에 상태 값 연결
+                onChange={handlePasswordChange} // 비밀번호 입력 변경 이벤트 핸들러
+              />
+            </LoginInputWrap>
+          </LoginInputContainer>
+          <LoginButtonWrap>
+            <button onClick={handleLogin}>Login</button>
+            <div>Forgot password?</div>
+          </LoginButtonWrap>
+        </LoginFormLeftSide>
+      </LoginFormContainer>
+    </Container>
   );
 };
 
-const LoginPageContainer = styled.div`
-  height: 100vh;
+const Container = styled.div`
   width: 100vw;
-  background-color: ${Colors.sub1};
+  height: 100vh;
+  background: #eff0f2;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 2rem;
 `;
 
-const LoginContainer = styled.div`
-  position: relative;
+const LoginFormContainer = styled.div`
+  background: #f5f5f5;
+  width: 50%;
+  height: 70%;
+  min-height: 600px;
+  min-width: 1000px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: row;
-  height: 80%;
-  width: 75%;
-  background-color: #fd9999;
+  box-shadow: 10px black;
+  border-radius: 10px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.228);
 `;
 
-const LoginImageWrapper = styled.div`
-  flex: 1 0 70%;
-  height: 100%;
-  width: auto;
-  position: relative;
-  background-color: ${Colors.sub1};
-`;
-
-const CompanyLogo = styled.div`
-  position: absolute;
-  top: 10px;
-  left: 10px;
+const LoginFormRightSide = styled.div`
+  width: 50%;
+  border-radius: 10px 0px 0px 10px;
+  padding: 75px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const LogoTitle = styled.h1`
-  position: absolute;
-  top: 30%;
-  left: 10%;
-  font-size: 2rem;
-`;
-
-const LogoSubtitle = styled.p`
-  position: absolute;
-  top: 50%;
-  left: 10%;
-  font-size: 1rem;
-`;
-
-const DummyImg = styled.img`
-  position: absolute;
-  width: 30%;
-  height: 80%;
-  right: 0;
-  bottom: 0;
-`;
-
-const LogoImg = styled.img`
-  width: 100%;
-  height: 100%;
-`;
-
-const LoginFormWrapper = styled.div`
-  flex: 1 0 30%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: auto;
-`;
-
-const LoginForm = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  display: grid;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  justify-items: center;
-  align-items: center;
-`;
-
-const LoginTitle = styled.h1`
-  font-size: 2rem;
-`;
-
-const LoginSmall = styled.p`
-  font-size: 1rem;
-`;
-
-const LoginInput = styled.input`
-  width: 80%;
-  height: 30px;
-  border: 1px solid black;
-`;
-
-const LoginButton = styled.button`
-  width: 80%;
-  height: 30px;
-  background-color: #000;
-  color: #fff;
-  border: none;
-`;
-
-const LoginOptions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 80%;
-`;
-
-const RememberCheckBox = styled.div`
   flex-direction: column;
-  background-color: #000;
-  color: #fff;
-  border: none;
-`;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-image: radial-gradient(
+    ellipse farthest-corner at 0 140%,
+    #001638 0%,
+    #637ca1 70%,
+    #637ca1d0 70%
+  );
 
-const CheckBox = styled.div`
-  width: 10%;
-  height: 10%;
-  background-color: #6c7d8f;
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #252e37;
+  .title {
+    color: white;
+    width: 100%;
+    text-align: right;
+    opacity: 0.9;
+    .logo {
+      width: 100%;
+    }
+  }
+  .description {
+    padding: 15px;
+    font-size: 2rem;
+    opacity: 0.5;
   }
 `;
 
-const ForgotPassword = styled.button`
-  background-color: #000;
-  color: #fff;
-  border: none;
+const LoginFormLeftSide = styled.div`
+  width: 50%;
+  border-radius: 0px 10px 10px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    287deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(243, 244, 244, 1) 0%,
+    rgba(255, 255, 255, 1) 100%
+  );
 `;
 
-const Empty = styled.div``;
+const LoginInputContainer = styled.div`
+  width: 300px;
+
+  .icon {
+    color: #001638;
+    line-height: 45px;
+  }
+
+  input {
+    background: none;
+    border: none;
+    line-height: 45px;
+    padding-left: 10px;
+    width: 267px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const LoginInputWrap = styled.div`
+  width: 300px;
+  height: 45px;
+  margin-top: 20px;
+  border-radius: 2px;
+  border-bottom: solid 2px #001638;
+`;
+
+const LoginButtonWrap = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  button {
+    cursor: pointer;
+    width: 120px;
+    height: 50px;
+    color: white;
+    border: 0;
+    border-radius: 4px;
+    font-size: 2rem;
+    background: rgb(105, 163, 255);
+    background: linear-gradient(162deg, #001638 0%, #637ca1 70%, #637ca1d0 70%);
+
+    &:hover {
+      opacity: 0.7;
+      transition: opacity 0.3s ease;
+    }
+  }
+  div {
+    margin-top: 10px;
+    text-decoration: none;
+    font-size: 11px;
+    color: gray;
+  }
+`;
 
 export default Login;
