443e3cc0213651798ca6a8e400bb9a250574625d
이정은 <silver36c@gmail.com>
2024-05-19 02:43:23 +0900

    🎨 Style: Reformat code

--- Backend/src/main/java/com/yes/trend/api/anonymousquestion/dto/AnonymousQuestionDto.java
+++ Backend/src/main/java/com/yes/trend/api/anonymousquestion/dto/AnonymousQuestionDto.java
@@ -1,9 +1,7 @@
 package com.yes.trend.api.anonymousquestion.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public class AnonymousQuestionDto {
 	@Getter
